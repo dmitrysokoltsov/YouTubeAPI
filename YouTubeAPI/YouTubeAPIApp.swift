0e1ed49d65eef6f145a99079d39c522a6678beea
DmitrySokoltsov <86969547+dmitrysokoltsov@users.noreply.github.com>
2022-05-10 20:00:11 +0300

    Initial Commit

---
+++ YouTubeAPI/YouTubeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouTubeAPIApp.swift
+//  YouTubeAPI
+//
+//  Created by Dmitry Sokoltsov on 10.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct YouTubeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
