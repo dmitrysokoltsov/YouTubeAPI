1dcd3899f30002184b8aaa459a88863c31a89bdb
DmitrySokoltsov <dmitrysokoltsov@gmail.com>
2022-05-10 21:38:18 +0300

    Refactoring

--- YouTubeAPI/View/ContentView.swift
+++ YouTubeAPI/View/ContentView.swift
@@ -14,11 +14,6 @@ struct ContentView: View {
         navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
     }
     
-    public let timer = Timer.publish(every:3,on:.main,in:.common).autoconnect()
-    @State private var selection = 0
-    
-    let album = ["one","two","thre","four"]
-    
     var body: some View {
         NavigationView {
             
@@ -26,23 +21,8 @@ struct ContentView: View {
                 Color("AccentColor").ignoresSafeArea()
                 
                 VStack {
-                TabView(selection : $selection){
+                    MainSlider()
                     
-                    ForEach(0..<4){ i in
-                        Image("\(album[i])")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                    }
-                }
-                .tabViewStyle(PageTabViewStyle())
-                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .never))
-                    .onReceive(timer, perform: { _ in
-                        withAnimation{
-                            print("selection is",selection)
-                            selection = selection < 4 ? selection + 1 : 0
-                        }
-                    })
-                
                     Text("Playlist name")
                         .fontWeight(.bold)
                         .foregroundColor(.white)
